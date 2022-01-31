9114480435cedfab8a12646b34158d5e899fa82c
geneaky <kaktus418@gmail.com>
2022-01-31 12:37:23 +0900

    feat&test pipeline

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -168,7 +171,98 @@ public class StreamTest {
         .map(Order::getCreatedByUserId)
         .collect(Collectors.toList());
     System.out.println(orderUserId);
+    //when
+    //then
+  }
 
+  @Test
+  public void pipelineTest() throws Exception {
+    //given
+    User user1 = new User()
+        .setId(101)
+        .setName("alice")
+        .setVerified(true)
+        .setEmailAdddress("alice@naver.com");
+    User user2 = new User()
+        .setId(102)
+        .setName("bob")
+        .setVerified(false)
+        .setEmailAdddress("bob@naver.com");
+    User user3 = new User()
+        .setId(103)
+        .setName("charlie")
+        .setVerified(false)
+        .setEmailAdddress("charlie@naver.com");
+    List<User> users = Arrays.asList(user1, user2, user3);
+
+    List<String> emails = new ArrayList<>();
+    for (User user : users) {
+      if (!user.isVerified()) {
+        emails.add(user.getEmailAdddress());
+      }
+    }
+
+    System.out.println(emails);
+
+    List<String> emails2 = users.stream()
+        .filter(user -> !user.isVerified())
+        .map(User::getEmailAdddress)
+        .collect(Collectors.toList());
+    System.out.println(emails2);
+
+    LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    Order order1 = new Order()
+        .setId(1001)
+        .setStatus(OrderStatus.CRAETED)
+        .setCratedAt(now.minusHours(4))
+        .setCreatedByUserId(101);
+    Order order2 = new Order()
+        .setId(1002)
+        .setStatus(OrderStatus.ERROR)
+        .setCratedAt(now.minusHours(1))
+        .setCreatedByUserId(103);
+    Order order3 = new Order()
+        .setId(1003)
+        .setStatus(OrderStatus.ERROR)
+        .setCratedAt(now.minusHours(36))
+        .setCreatedByUserId(102);
+    Order order4 = new Order()
+        .setId(1004)
+        .setStatus(OrderStatus.IN_PROGRESS)
+        .setCratedAt(now.minusHours(15))
+        .setCreatedByUserId(104);
+    Order order5 = new Order()
+        .setId(1005)
+        .setStatus(OrderStatus.PROCESSED)
+        .setCratedAt(now.minusHours(10))
+        .setCreatedByUserId(102);
+
+    List<Order> orders = Arrays.asList(order1, order2, order3, order4, order5);
+
+    List<Long> userIds = orders.stream()
+        .filter(oder -> oder.getStatus() == OrderStatus.ERROR)
+        .map(Order::getCreatedByUserId)
+        .collect(Collectors.toList());
+    System.out.println(userIds);
+
+    List<Long> userIds2 = orders.stream()
+        .filter(order -> order.getStatus() == OrderStatus.ERROR)
+        .filter(order -> order.getCratedAt().isAfter(now.minusHours(24)))
+        .map(Order::getCreatedByUserId)
+        .collect(Collectors.toList());
+    System.out.println(userIds2);
+    //when
+    //then
+  }
+
+  @Test
+  public void sortTest() throws Exception {
+    //given
+    List<Integer> numbers = Arrays.asList(3, -5, 7, 4);
+    List<Integer> sortedNumbers = numbers.stream()
+        .sorted()
+        .collect(Collectors.toList());
+    System.out.println(sortedNumbers);
     //when
     //then
   }
