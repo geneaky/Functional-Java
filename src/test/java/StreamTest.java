87fd727122d4326fae1581d35e4ce46ad27f29dd
geneaky <kaktus418@gmail.com>
2022-01-31 12:51:55 +0900

    feat&test distinct

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -323,4 +323,52 @@ public class StreamTest {
     //then
   }
 
+  @Test
+  public void distinctTest() throws Exception {
+    //given
+    List<Integer> numbers = Arrays.asList(3, -5, 4, -5, 2, 3);
+    List<Integer> distinctNumbers = numbers.stream()
+        .distinct()
+        .collect(Collectors.toList());
+    System.out.println(distinctNumbers);
+    LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    Order order1 = new Order()
+        .setId(1001)
+        .setStatus(OrderStatus.CRAETED)
+        .setCratedAt(now.minusHours(4))
+        .setCreatedByUserId(101);
+    Order order2 = new Order()
+        .setId(1002)
+        .setStatus(OrderStatus.ERROR)
+        .setCratedAt(now.minusHours(1))
+        .setCreatedByUserId(103);
+    Order order3 = new Order()
+        .setId(1003)
+        .setStatus(OrderStatus.ERROR)
+        .setCratedAt(now.minusHours(36))
+        .setCreatedByUserId(102);
+    Order order4 = new Order()
+        .setId(1004)
+        .setStatus(OrderStatus.IN_PROGRESS)
+        .setCratedAt(now.minusHours(15))
+        .setCreatedByUserId(104);
+    Order order5 = new Order()
+        .setId(1005)
+        .setStatus(OrderStatus.PROCESSED)
+        .setCratedAt(now.minusHours(10))
+        .setCreatedByUserId(102);
+
+    List<Order> orders = Arrays.asList(order1, order2, order3, order4, order5);
+
+    List<Long> userIds = orders.stream()
+        .map(Order::getCreatedByUserId)
+        .distinct()
+        .sorted()
+        .collect(Collectors.toList());
+
+    System.out.println(userIds);
+    //when
+    //then
+  }
+
 }
