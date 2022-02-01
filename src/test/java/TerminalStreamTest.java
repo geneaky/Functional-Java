7da875b159c3092321195884637ece036822d9c7
geneaky <kaktus418@gmail.com>
2022-02-01 18:04:29 +0900

    feat&test collectors grouping by

--- src/test/java/TerminalStreamTest.java
+++ src/test/java/TerminalStreamTest.java
@@ -474,4 +474,56 @@ public class TerminalStreamTest {
     //then
   }
 
+  @Test
+  public void groupingBy() throws Exception {
+    //given
+    List<Integer> numbers = Arrays.asList(13, 2, 101, 203, 304, 402, 305, 349, 2312, 203);
+    Map<Integer, List<Integer>> unitDigitMap = numbers.stream()
+        .collect(Collectors.groupingBy(number -> number % 10));
+
+    Map<Integer, Set<Integer>> unitDigitSet = numbers.stream()
+        .collect(Collectors.groupingBy(number -> number % 10, Collectors.toSet()));
+
+    Map<Integer, List<String>> unitDigitStrMap = numbers.stream()
+        .collect(Collectors.groupingBy(number -> number % 10,
+            Collectors.mapping(number -> "unit digit is " + number, Collectors.toList())));
+
+    Order order1 = new Order()
+        .setId(1001)
+        .setAmount(BigDecimal.valueOf(3000))
+        .setStatus(OrderStatus.CRAETED);
+    Order order2 = new Order()
+        .setId(1002)
+        .setAmount(BigDecimal.valueOf(12000))
+        .setStatus(OrderStatus.ERROR);
+    Order order3 = new Order()
+        .setId(1003)
+        .setAmount(BigDecimal.valueOf(4000))
+        .setStatus(OrderStatus.ERROR);
+    Order order4 = new Order()
+        .setId(1004)
+        .setAmount(BigDecimal.valueOf(7000))
+        .setStatus(OrderStatus.IN_PROGRESS);
+    Order order5 = new Order()
+        .setId(1005)
+        .setAmount(BigDecimal.valueOf(1000))
+        .setStatus(OrderStatus.PROCESSED);
+
+    List<Order> orders = Arrays.asList(order1, order2, order3, order4, order5);
+
+    Map<OrderStatus, List<Order>> orderStatusMap = orders.stream()
+        .collect(Collectors.groupingBy(Order::getStatus));
+
+    Map<OrderStatus, BigDecimal> orderStatusToSumOfAmountMap = orders.stream()
+        .collect(Collectors.groupingBy(Order::getStatus, Collectors.mapping(Order::getAmount,
+            Collectors.reducing(BigDecimal.ZERO, BigDecimal::add))));
+    //when
+    System.out.println(unitDigitMap);
+    System.out.println(unitDigitSet);
+    System.out.println(unitDigitStrMap);
+    System.out.println(orderStatusMap);
+    System.out.println(orderStatusToSumOfAmountMap);
+    //then
+  }
+
 }
