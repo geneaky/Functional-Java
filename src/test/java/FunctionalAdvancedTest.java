caef3fefe3c2774e5da9cf34ca9cf10ac1ce69ba
geneaky <kaktus418@gmail.com>
2022-02-02 16:58:09 +0900

    feat&test lazy evaluation

--- src/test/java/FunctionalAdvancedTest.java
+++ src/test/java/FunctionalAdvancedTest.java
@@ -1,8 +1,11 @@
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -33,6 +36,54 @@ public class FunctionalAdvancedTest {
     };
 
     return supplier;
+  }
+
+  @Test
+  public void lazyEvaluationTest() throws Exception {
+    //given
+    if (returnTrue() || returnFalse()) {
+      System.out.println("true");
+    }
+
+    if (or(returnTrue(), returnFalse())) {
+      System.out.println("true");
+    }
+
+    if (lazyOr(() -> returnTrue(), () -> returnFalse())) {
+      System.out.println("lazyTrue");
+    }
+
+    //stream은 종결 처리가 이루어질때까지 연산을 미룬다.
+    Stream<Integer> intergerStream = Stream.of(3,-2,5,8,-3,10)
+        .filter(x -> x > 0)
+        .peek(x -> System.out.println(x))
+        .filter(x -> x % 2 == 0);
+    System.out.println("Before collect");
+
+    List<Integer> integers = intergerStream.collect(Collectors.toList());
+
+    System.out.println("After collect: " + integers);
+
+    //when
+    //then
+  }
+
+  public static boolean or(boolean x, boolean y) {
+    return x||y;
+  }
+
+  public static boolean lazyOr(Supplier<Boolean> x, Supplier<Boolean> y) {
+    return x.get() || y.get();
+  }
+
+  public static boolean returnTrue() {
+    System.out.println("Returning true");
+    return true;
+  }
+
+  public static boolean returnFalse() {
+    System.out.println("Returning false");
+    return false;
   }
 
 }
